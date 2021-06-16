7fa7534e5d9fde6253564deb099e6abc7c905c3a
Luis Pico <ext_luiad@arlafoods.com>
2021-06-16 16:15:35 +0200

    added deployment script (PowerShell)

---
+++ Deployment/azDeployStorageAccount.ps1
@@ -0,0 +1,27 @@
+#Requires -Version 3.0
+#Requires -Module Az.Resources
+#Requires -Module Az.Storage
+
+$deploymentScope = 'ResourceGroup'
+$Location = 'westeurope'
+$ResourceGroupName = 'rg-lp-testLab'
+
+# Create the resource group only when it doesn't already exist - and only in RG scoped deployments
+if ($deploymentScope -eq "ResourceGroup") {
+    if ((Get-AzResourceGroup -Name $ResourceGroupName -Location $Location -Verbose -ErrorAction SilentlyContinue) -eq $null) {
+        New-AzResourceGroup -Name $ResourceGroupName -Location $Location -Verbose -Force -ErrorAction Stop
+    }
+}
+
+#Chech if storage account already exist
+$StorageAccountName = 'azlpsatl'
+
+$StorageAccount = (Get-AzStorageAccount | Where-Object { $_.StorageAccountName -eq $StorageAccountName })
+
+# Create the storage account if it doesn't already exist
+if ($StorageAccount -eq $null) {
+    if ((Get-AzResourceGroup -Name $ResourceGroupName -Verbose -ErrorAction SilentlyContinue) -eq $null) {
+        New-AzResourceGroup -Name $ResourceGroupName -Location $Location -Verbose -Force -ErrorAction Stop
+    }
+    ew-AzResourceGroupDeployment -ResourceGroupName $ResourceGroupName -TemplateFile '../templates/azuredeploy.json' -TemplateParameterFile '../templates/azuredeploy.parameters.json'
+}
